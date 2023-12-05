ceac19a679ecfb5046073c5e5004d7fa425afa9b
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2023-12-05 23:24:10 +0900

    feat: Wrote an answer

---
+++ primitiveType.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * A. こんにちは
+ * 文字列型　      string
+ * 
+ * B. true
+ * 論理型　        boolean
+ * 
+ * C. 14
+ * 整数型          int
+ * 
+ * D. 9.34
+ * 浮動小数点数型　 float
+ * 
+ * E. 1.0E-18
+ * 浮動小数点数型　 double
+ * 
+ * F. 0
+ * null型　        null
+ */
