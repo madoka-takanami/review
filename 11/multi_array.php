93160f9703bafc96b973464e58679308f732c57d
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2023-12-05 17:59:02 +0900

    feat: Attached how to declare multi array

---
+++ 11/multi_array.php
@@ -0,0 +1,14 @@
+<?php
+
+const STATIONS = [
+    ['東京', '秋葉原', '高田馬場'], 
+    ['渋谷', '中央林間', '南町田グランベリーパーク']
+];
+
+echo STATIONS[0][0] . "\n";
+echo STATIONS[0][1] . "\n";
+echo STATIONS[0][2] . "\n";
+echo STATIONS[1][0] . "\n";
+echo STATIONS[1][1] . "\n";
+echo STATIONS[1][2] . "\n";
+
