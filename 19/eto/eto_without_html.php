644ae3e1bb4dc9bcf7d70b836de47e4dbac40161
OMZ <105539693+Ometeor-Zheero-OMZ@users.noreply.github.com>
2023-12-06 10:27:29 +0900

    不要なコメントアウトを消しました。

--- 19/eto/eto_without_html.php
+++ 19/eto/eto_without_html.php
@@ -9,9 +9,9 @@ declare(strict_types=1);
 const ETO_LIST_ARR = ['午年', '未年', '申年', '酉年', '戌年', '亥年', '子年', '丑年', '寅年', '卯年', '辰年', '巳年']; // 0 - 11
 define('CNT_ETO_LIST', count(ETO_LIST_ARR));
 
-define('END_YEAR', intval(date('Y'))); // 2023
-const START_YEAR = END_YEAR - 33; // 1990
-const TOTAL_YEAR = END_YEAR - START_YEAR; // 33
+define('END_YEAR', intval(date('Y')));
+const START_YEAR = END_YEAR - 33;
+const TOTAL_YEAR = END_YEAR - START_YEAR;
 
 
 for ($index = 0, $diff_year = 0; $index < TOTAL_YEAR; $index++, $diff_year++) {
