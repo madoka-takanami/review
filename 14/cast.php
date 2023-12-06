ba986f740b8dd5f08ea136c0675bfe3ddfc5575f
OMZ <105539693+Ometeor-Zheero-OMZ@users.noreply.github.com>
2023-12-06 10:14:06 +0900

    不要なコメントアウトを消しました。

--- 14/cast.php
+++ 14/cast.php
@@ -1,19 +1,11 @@
 <?php
 
-/**
- * ファストスに記載されているサンプルコードです。
- * 物足りないので少し書き換えました。
- */
-
 const STR_NUM1 = '12345';
 const STR_NUM2 = '54321';
-
-// 整数型に変換してください。
 
 var_dump(STR_NUM1);
 var_dump(STR_NUM2);
 
-//
 define('RESULT', intval(STR_NUM1) + STR_NUM2);
 
 echo intval(STR_NUM1) . " + " . STR_NUM2 . " = " . RESULT;
