a3a4a23ccccb889f3f190c6cbcc6f8bb6e0a7453
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2023-12-06 14:33:43 +0900

    fix: Commenced from sunday to saturaday at the end

--- 03/calender.php
+++ 03/calender.php
@@ -2,14 +2,14 @@
 
 for ($day = 1; $day <= 31; $day++) {
 
-    $is_saturday = ($day % 7 === 6);
+    $is_saturday = ($day % 7 === 1);
 
     if ($is_saturday)
     {
-        echo '<span style="color: blue; padding: 1rem">' . $day . '</span>';
+        echo '<span style="color: red; padding: 1rem">' . $day . '</span>';
     } else if ($day % 7 === 0)
     {
-        echo '<span style="color: red; padding: 1rem">' . $day . '</span><br>';
+        echo '<span style="color: blue; padding: 1rem">' . $day . '</span><br>';
     }
     else
     {
