f24622ee4796c794ec0c7dead17a93ec12763743
OMZ <105539693+Ometeor-Zheero-OMZ@users.noreply.github.com>
2023-12-06 10:19:05 +0900

    不要なコメントアウトを消しました。

--- 19/primes/config-prime_numbers.php
+++ 19/primes/config-prime_numbers.php
@@ -1,12 +1,6 @@
 <?php
 
 declare(strict_types=1);
-
-// 素数を100までとした最大値です。
 const LIMIT = 100;
-
-// 素数を格納した配列です。
 define('PRIMES_ARR', findPrimes(LIMIT));
-
-// 配列の要素数です。
 define('PRIMES_ARR_LENGTH', count(PRIMES_ARR)); // 25
